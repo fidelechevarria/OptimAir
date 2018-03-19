75d426551526dcde5b866046621bf242bd7bb74e
Fidel Echevarría <fechevarria@uavnavigation.com>
2018-03-19 09:14:05 +0100

    Changed to fmincon without parallel to test real-time plotting.

--- optimTraj_optimize.m
+++ optimTraj_optimize.m
@@ -3,14 +3,15 @@ function [x,fval,x_G,fval_G] = optimTraj_optimize(WP,hdngData)
 %     % Perform initial optimization using a genetic algorithm
 %     [x_G,fval_G,~,~,~,~] = optimTraj_genetic(WP,hdngData);
     
-%     % Perform final optimization using gradient-descent
-%     [x,fval,~,~,~,~,~] = optimTraj_fmincon(x_G,WP,hdngData);
+    % Perform final optimization using gradient-descent
+    x_G = 0;
+    [x,fval,~,~,~,~,~] = optimTraj_fmincon(x_G,WP,hdngData);
     
 %     % Perform global optimization using multiStart fmincon in a parallel pool
 %     [x,fval] = optimTraj_multiStartParallel(WP,hdngData);
 
-    % Perform global optimization using pattern search
-    [x,fval] = optimTraj_patternSearch(WP,hdngData);
+%     % Perform global optimization using pattern search
+%     [x,fval] = optimTraj_patternSearch(WP,hdngData);
     
     % Show optimization results
     automaticFGlaunchIsActivated = 0;
