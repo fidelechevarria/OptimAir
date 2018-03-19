97ca3505c3b84172968be855806d5689176cfdba
Fidel Echevarría <fechevarria@uavnavigation.com>
2018-03-19 09:08:10 +0100

    Modified curvature equation for 2D special case

--- optimTraj_ppcurv.m
+++ optimTraj_ppcurv.m
@@ -8,15 +8,19 @@ firstDer = fnder(smoothTraj,1);
 firstDerVal = fnval(firstDer,space);
 firstDer_north = firstDerVal(1,:)';
 firstDer_east = firstDerVal(2,:)';
-firstDer_up = firstDerVal(3,:)';
+% firstDer_up = firstDerVal(3,:)';
 
 secondDer = fnder(smoothTraj,2);
 secondDerVal = fnval(secondDer,space);
 secondDer_north = secondDerVal(1,:)';
 secondDer_east = secondDerVal(2,:)';
-secondDer_up = secondDerVal(3,:)';
+% secondDer_up = secondDerVal(3,:)';
 
-curvature = sqrt((secondDer_up.*firstDer_east-secondDer_east.*firstDer_up).^2+(secondDer_north.*firstDer_up-secondDer_up.*firstDer_north).^2+(secondDer_east.*firstDer_north-secondDer_north.*firstDer_east).^2)./...
-    ((firstDer_north.^2+firstDer_east.^2+firstDer_up.^2).^1.5);
+curvature = sqrt((secondDer_east.*firstDer_north-secondDer_north.*firstDer_east).^2)./...
+    ((firstDer_north.^2+firstDer_east.^2).^1.5);
+
+% For 3D
+% curvature = sqrt((secondDer_up.*firstDer_east-secondDer_east.*firstDer_up).^2+(secondDer_north.*firstDer_up-secondDer_up.*firstDer_north).^2+(secondDer_east.*firstDer_north-secondDer_north.*firstDer_east).^2)./...
+%     ((firstDer_north.^2+firstDer_east.^2+firstDer_up.^2).^1.5);
 
 end
