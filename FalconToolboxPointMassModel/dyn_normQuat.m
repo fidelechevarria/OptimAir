a5e628db70de6497c06d1dc7364611c3fcb9669b
Fidel Echevarría <fechevarria@uavnavigation.com>
2018-06-02 14:03:59 +0200

    Modified and renamed "dyn_normQuat" (currently not used).

--- FalconToolboxPointMassModel/dyn_normQuat.m
+++
@@ -1,7 +0,0 @@
-function [q0_norm,q1_norm,q2_norm,q3_norm] = dyn_normQuat(q0,q1,q2,q3)
-q_norm = sqrt(q0.^2 + q1.^2 + q2.^2 + q3.^2);
-q0_norm = q0./q_norm;
-q1_norm = q1./q_norm;
-q2_norm = q2./q_norm;
-q3_norm = q3./q_norm;
-end
